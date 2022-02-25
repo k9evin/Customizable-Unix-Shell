2ae7dd777b7f160482a4600db4d772d912d2b80e
Jiayue Lin <kevin5719927576@gmail.com>
2022-02-24 19:40:10 -0500

    update custom_prompt test

--- src/custom_prompt_test.py
+++ src/custom_prompt_test.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python
 #
 # Tests the functionality of custom prompt implement
-# Also serves as example of how to write your own
-# custom functionality tests.
-#
+
 import atexit, proc_check, time
 from testutils import *
 
@@ -12,5 +10,5 @@ console = setup_tests()
 # ensure that shell prints expected prompt
 #setup
 sendline("")
-expect_prompt("<[^@]*@[^>]*>\$")
+expect("<[^@]*@[^>]*>\$")
 test_success()
